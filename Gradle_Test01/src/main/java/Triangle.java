ffc1341a0778eddc1f28f5ce947de9b32f0b3c44
ZFTBN <2574771691@qq.com>
2023-03-20 19:28:46 +0800

    第二次实验

--- Gradle_Test01/src/main/java/Triangle.java
+++ Gradle_Test01/src/main/java/Triangle.java
@@ -2,12 +2,14 @@ public class Triangle {
 
     public String classify(int a, int b, int c) {
 
-        if (!((a + b > c) && (a + c > b) && (b + c > a))) {
+        if (a < 1 || b < 1 || c < 1 | a > 100 | b > 100 | c > 100)
+            return "输入错误";
+        else if (!((a + b > c) && (a + c > b) && (b + c > a))) {
             return "非三角形";
         } else if (a == b && a == c && b == c) {
             return "等边三角形";
         } else if (a != b && a != c && b != c) {
-            return "不等边三角形";
+            return "一般三角形";
         } else {
             return "等腰三角形";
         }
--- Gradle_Test01/src/test/java/TriangleTest.java
+++ Gradle_Test01/src/test/java/TriangleTest.java
@@ -3,24 +3,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import java.text.ParseException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class TriangleTest {
 
     @Test
-    @DisplayName(value="不等边三角形")
+    @DisplayName(value="一般三角形")
     void testTriangle() {
         Triangle triangle = new Triangle();
 
         String type = triangle.classify(3, 4, 6);
-        assertEquals("不等边三角形", type);
+        assertEquals("一般三角形", type);
     }
 
     @Test
+    @DisplayName(value="等边三角形")
     void equallaterialTriangle() {
         Triangle triangle = new Triangle();
 
@@ -29,6 +28,7 @@ class TriangleTest {
     }
 
     @Test
+    @DisplayName(value="非三角形")
     void notTriangle() {
         Triangle triangle = new Triangle();
 
@@ -38,7 +38,7 @@ class TriangleTest {
 
     @ParameterizedTest
     @CsvSource({
-            "3,4,6,不等边三角形",
+            "3,4,6,一般三角形",
             "3,3,3,等边三角形",
             "3,3,6,非三角形"
     })
@@ -60,9 +60,9 @@ class TriangleTest {
 
         assertEquals(expected, type);
     }
-    @DisplayName(value="三角形健壮性最坏情况测试用例")
+    @DisplayName(value="三角形健壮性边界测试用例")
     @ParameterizedTest
-    @CsvFileSource(resources = "/三角形健壮性最坏情况测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    @CsvFileSource(resources = "/三角形健壮性边界测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
     void fileTest1(int m,int a, int b,int c,String expected) throws ParseException {
 
         Triangle triangle=new Triangle();
@@ -71,9 +71,9 @@ class TriangleTest {
 
         assertEquals(expected, type);
     }
-    @DisplayName(value="三角形健壮测试用例")
+    @DisplayName(value="三角形最坏情况一般边界测试用例")
     @ParameterizedTest
-    @CsvFileSource(resources = "/三角形健壮测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    @CsvFileSource(resources = "/三角形最坏情况一般边界测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
     void fileTest2(int m,int a, int b,int c,String expected) throws ParseException {
 
         Triangle triangle=new Triangle();
@@ -82,20 +82,9 @@ class TriangleTest {
 
         assertEquals(expected, type);
     }
-    @DisplayName(value="三角形弱一般等价类测试用例")
+    @DisplayName(value="三角形最坏情况健壮性边界测试用例")
     @ParameterizedTest
-    @CsvFileSource(resources = "/三角形弱一般等价类测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
-    void fileTest3(int m,int a, int b,int c,String expected) throws ParseException {
-
-        Triangle triangle=new Triangle();
-
-        String type = triangle.classify(a, b, c);
-
-        assertEquals(expected, type);
-    }
-    @DisplayName(value="三角形最坏情况测试用例")
-    @ParameterizedTest
-    @CsvFileSource(resources = "/三角形最坏情况测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    @CsvFileSource(resources = "/三角形最坏情况健壮性边界测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
     void fileTest4(int m,int a, int b,int c,String expected) throws ParseException {
 
         Triangle triangle=new Triangle();
